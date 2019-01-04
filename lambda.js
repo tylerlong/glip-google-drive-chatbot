7a486f9eb71f3102a93ba3fa8bbe82749eebcbb0
Tyler Long <tyler4long@gmail.com>
2019-01-04 14:05:43 +0800

    Try Lambda

--- lambda.js
+++ lambda.js
@@ -1,14 +1,19 @@
+import serverlessHTTP from 'serverless-http'
+import { createAsyncProxy } from 'ringcentral-chatbot/dist/lambda'
+import axios from 'axios'
 import createApp from 'ringcentral-chatbot/dist/apps'
 import googleDriveSkill from 'ringcentral-chatbot-skill-google-drive'
-import axios from 'axios'
 
 const app = createApp(undefined, [googleDriveSkill])
-app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
 
-setInterval(async () => {
+module.exports.app = serverlessHTTP(app)
+
+module.exports.proxy = createAsyncProxy('app')
+
+module.exports.maintain = async () => {
   await axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`, undefined, { auth: {
     username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
     password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
   } })
   await axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/google/refresh-subscriptions`)
-}, 3600000)
+}

f108c2c2e23ac1dc319ee05185462ba592e24637
Tyler Long <tyler4long@gmail.com>
2019-01-04 13:53:16 +0800

    Finish the express code

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 import createApp from 'ringcentral-chatbot/dist/apps'
 import googleDriveSkill from 'ringcentral-chatbot-skill-google-drive'
+import axios from 'axios'
 
 const handle = async event => {
   // event handling code
@@ -9,3 +10,5 @@ const app = createApp(handle, [
   googleDriveSkill
 ])
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
+
+setInterval(() => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/google/refresh-subscriptions`), 3600000)

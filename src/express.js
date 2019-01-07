1a2b22a4ce21c5bbf2fc8f6be8eef6af259ba024
Tyler Liu <tyler.liu@lmxmn085.rcoffice.ringcentral.com>
2019-01-07 16:06:22 +0800

    Express.js google site verification

--- src/express.js
+++ src/express.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 import app from './app'
 
+// for google site verification
+app.get(`/${process.env.GOOGLE_SITE_VERIFICATION}`, (req, res) => {
+  res.send(`google-site-verification: ${process.env.GOOGLE_SITE_VERIFICATION}`)
+})
+
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
 
 setInterval(async () => {

dcbec5a6f9e98bb33504c9947b54ced1860b37e7
Tyler Liu <tyler.liu@lmxmn085.rcoffice.ringcentral.com>
2019-01-07 15:41:50 +0800

    For lambda google-site-verification

--- src/lambda.js
+++ src/lambda.js
@@ -1,12 +1,19 @@
 import serverlessHTTP from 'serverless-http'
 import { createAsyncProxy } from 'ringcentral-chatbot/dist/lambda'
 import axios from 'axios'
+import express from 'express'
 
 import app from './app'
 
 module.exports.app = serverlessHTTP(app)
 
-module.exports.proxy = createAsyncProxy('app')
+// for google site verification
+const filterApp = express()
+filterApp.get('/google/webhook/googlece5a6e1d27e7fa99.html', (req, res) => {
+  res.send('google-site-verification: googlece5a6e1d27e7fa99.html')
+})
+
+module.exports.proxy = createAsyncProxy('app', filterApp)
 
 module.exports.maintain = async () => {
   await axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`, undefined, { auth: {
